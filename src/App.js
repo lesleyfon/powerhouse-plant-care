be3a731ee634d2b51f3fe4b04b67a42426b388f0
lesley <lesleybanadzem2017@gmail.com>
2021-04-13 13:48:37 -0700

    Created a basic header **Work on the header in the next iteration **

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Header from "./Component/Header/Header";
 
 function App() {
-	return <main className="App">main</main>;
+	return (
+		<main className="App">
+			<Header />
+		</main>
+	);
 }
 
 export default App;
