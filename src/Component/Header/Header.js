4f2b8c903cdaa0081d168075f517e1499f3e601d
lesley <lesleybanadzem2017@gmail.com>
2021-04-13 17:18:10 -0700

    created a nvabar and Added styles for the navigation bar

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,163 +1,185 @@
 import React from "react";
+import PlusSvg from "./../../assets/svgs/plusSign.svg";
+import "./Header.css";
+class Header extends React.Component {
+	constructor() {
+		super();
+		this.state = {};
+	}
+	s;
 
-function Header() {
-	return (
-		<>
-			{/* <!-- This example requires Tailwind CSS v2.0+ --> */}
-			<nav className="bg-gray-800">
-				<div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-					<div className="relative flex items-center justify-between h-16">
-						<div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-							{/* <!-- Mobile menu button--> */}
-							<button
-								type="button"
-								className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-								aria-controls="mobile-menu"
-								aria-expanded="false"
-							>
-								<span className="sr-only">Open main menu</span>
-								{/* <!-- Icon when menu is closed. Hero icon name: outline/menu Menu open: "hidden", Menu closed: "block"--> */}
-								<svg
-									className="block h-6 w-6"
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 24 24"
-									stroke="currentColor"
-									aria-hidden="true"
+	render() {
+		return (
+			<>
+				{/* <!-- This example requires Tailwind CSS v2.0+ --> */}
+				<nav className="border-b-2 border-th-secondary">
+					<div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+						<div className="relative flex items-center justify-between h-16">
+							<div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+								{/* <!-- Mobile menu button--> */}
+								<button
+									type="button"
+									className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+									aria-controls="mobile-menu"
+									aria-expanded="false"
 								>
-									<path
-										strokeLinecap="round"
-										strokeLinejoin="round"
-										strokeWidth="2"
-										d="M4 6h16M4 12h16M4 18h16"
-									/>
-								</svg>
-								{/* <!-- Icon when me nu is open. Heroicon name: outline/xMenu open: "block", Menu closed: "hidden"--> */}
-								<svg
-									className="hidden h-6 w-6"
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 24 24"
-									stroke="currentColor"
-									aria-hidden="true"
-								>
-									<path
-										strokeLinecap="round"
-										strokeLinejoin="round"
-										strokeWidth="2"
-										d="M6 18L18 6M6 6l12 12"
-									/>
-								</svg>
-							</button>
-						</div>
-						<div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-							<div className="flex-shrink-0 flex items-center">
-								<img
-									className="block lg:hidden h-8 w-auto"
-									src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
-									alt="Workflow"
-								/>
-								<img
-									className="hidden lg:block h-8 w-auto"
-									src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
-									alt="Workflow"
-								/>
+									<span className="sr-only">Open main menu</span>
+									{/* <!-- Icon when menu is closed. Hero icon name: outline/menu Menu open: "hidden", Menu closed: "block"--> */}
+									<svg
+										className="block h-6 w-6"
+										xmlns="http://www.w3.org/2000/svg"
+										fill="none"
+										viewBox="0 0 24 24"
+										stroke="currentColor"
+										aria-hidden="true"
+									>
+										<path
+											strokeLinecap="round"
+											strokeLinejoin="round"
+											strokeWidth="2"
+											d="M4 6h16M4 12h16M4 18h16"
+										/>
+									</svg>
+									{/* <!-- Icon when menu is open. Heroicon name: outline/xMenu open: "block", Menu closed: "hidden"--> */}
+									<svg
+										className="hidden h-6 w-6"
+										xmlns="http://www.w3.org/2000/svg"
+										fill="none"
+										viewBox="0 0 24 24"
+										stroke="currentColor"
+										aria-hidden="true"
+									>
+										<path
+											strokeLinecap="round"
+											strokeLinejoin="round"
+											strokeWidth="2"
+											d="M6 18L18 6M6 6l12 12"
+										/>
+									</svg>
+								</button>
 							</div>
-							<div className="hidden sm:block sm:ml-6">
-								<div className="flex space-x-4">
-									{/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
-									<a
-										href="/"
-										className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
-										aria-current="page"
-									>
-										About App
-									</a>
+							<div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+								<div className="flex-shrink-0 flex items-center">
+									<img
+										className="block lg:hidden h-8 w-auto"
+										src="https://www.logodesign.net/logo/abstract-line-farm-and-plant-inside-the-circle-with-sun-886ld.png?size=2&industry=plant&bg=0"
+										alt="Workflow"
+									/>
+									<img
+										className="hidden lg:block h-10 w-auto"
+										// src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
+										src="https://www.logodesign.net/logo/abstract-line-farm-and-plant-inside-the-circle-with-sun-886ld.png?size=2&industry=plant&bg=0"
+										alt="Workflow"
+									/>
+								</div>
+								<div className="hidden sm:block sm:ml-6">
+									<div className="flex space-x-4">
+										{/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
+										<a
+											href="/"
+											className=" text-th-primary text-base px-3 py-2 rounded-md text-sm font-medium hover:bg-th-light hover:text-th-green"
+											aria-current="page"
+										>
+											About App
+										</a>
 
-									<a
-										href="/"
-										className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-									>
-										Saved Plants
-									</a>
+										<a
+											href="/"
+											// className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+											className=" text-th-primary text-base px-3 py-2 rounded-md text-sm font-medium hover:bg-th-light hover:text-th-green"
+										>
+											Saved Plants
+										</a>
 
-									<a
-										href="/"
-										className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-									>
-										Plant Of The Day
-									</a>
+										<a
+											href="/"
+											// className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+											className=" text-th-primary text-base px-3 py-2 rounded-md text-sm font-medium hover:bg-th-light hover:text-th-green"
+										>
+											Plant Of The Day
+										</a>
 
-									<a
-										href="/"
-										className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-									>
-										Add Plant
-									</a>
+										<a
+											href="/"
+											className="
+                                            inline-flex
+                                            items-center
+                                            justify-center
+                                            space-x-2 
+                                            px-3
+                                            py-2
+                                            rounded-md 
+                                            text-sm 
+                                            font-medium 
+                                            hover:bg-th-light 
+                                            hover:text-th-green
+                                            rounded-md
+                                            text-th-primary
+                                            text-base 
+                                            hover:text-white 
+                                            hover:bg-gray-700 
+                                            focus:outline-none
+                                            focus:ring-2 
+                                            focus:ring-inset 
+                                            focus:ring-white"
+										>
+											<span>Add Plant </span>
+
+											<img
+												src={PlusSvg}
+												className="max-h-5 w-5 pr-1.5"
+												alt="Visual add sign"
+											/>
+										</a>
+									</div>
 								</div>
 							</div>
-						</div>
-						<div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-							<button className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-								<span className="sr-only">Search Saved Plants</span>
-								{/* <!-- Heroicon name: outline/bell --> */}
-								<svg
-									className="h-6 w-6"
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 24 24"
-									stroke="currentColor"
-									aria-hidden="true"
-								>
-									<path
-										strokeLinecap="round"
-										strokeLinejoin="round"
-										strokeWidth="2"
-										d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-									/>
-								</svg>
-							</button>
+							<div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+								<button className="bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+									Search Plants
+								</button>
+							</div>
 						</div>
 					</div>
-				</div>
 
-				{/* <!-- Mobile menu, show/hide based on menu state. --> */}
-				<div className="sm:hidden" id="mobile-menu">
-					<div className="px-2 pt-2 pb-3 space-y-1">
-						{/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
-						<a
-							href="/"
-							className="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium"
-							aria-current="page"
-						>
-							About App
-						</a>
+					{/* <!-- Mobile menu, show/hide based on menu state. --> */}
+					<div className="sm:hidden" id="mobile-menu">
+						<div className="px-2 pt-2 pb-3 space-y-1">
+							{/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
+							<a
+								href="/"
+								className="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium"
+								aria-current="page"
+							>
+								About App
+							</a>
 
-						<a
-							href="/"
-							className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-						>
-							Saved Plants
-						</a>
+							<a
+								href="/"
+								className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+							>
+								Saved Plants
+							</a>
 
-						<a
-							href="/"
-							className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-						>
-							Plant Of The Day
-						</a>
+							<a
+								href="/"
+								className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+							>
+								Plant Of The Day
+							</a>
 
-						<a
-							href="/"
-							className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-						>
-							Add Plant
-						</a>
+							<a
+								href="/"
+								className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+							>
+								Add Plant
+							</a>
+						</div>
 					</div>
-				</div>
-			</nav>
-		</>
-	);
+				</nav>
+			</>
+		);
+	}
 }
 
 export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@ module.exports = {
 	purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
-		extend: {},
+		colors: {
+			green: {
+				primary: "#25523B",
+				secondary: "#7c9789",
+				hover: "#92a89d",
+			},
+			white: {
+				lightGreen: "#e9edeb",
+				deepGreen: "#0b1811",
+				bgColor: "#ffffff",
+			},
+			"th-primary": "var(--primary)",
+			"th-secondary": "var(--secondary)",
+			"th-light": "var(--lightGreen)",
+			"th-green": "var(--deepGreen)",
+		},
 	},
 	variants: {
 		extend: {},
