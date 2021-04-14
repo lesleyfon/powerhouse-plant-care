b97da6876871ff4495afe7b186fc510620404c30
lesley <lesleybanadzem2017@gmail.com>
2021-04-13 18:56:13 -0700

    Created a route for the login and home page and a component for the logInpage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
 import Header from "./Component/Header/Header";
+import { Switch, Route } from "react-router-dom";
+import LogInSignUp from "./Component/Login/LogInSignUp";
 
 function App() {
 	return (
 		<main className="App">
-			<Header />
+			<Switch>
+				<Route path="/login" exact component={LogInSignUp} />
+				<Route path="/home" exact component={Header} />
+			</Switch>
 		</main>
 	);
 }
---
+++ src/Component/Login/LogInSignUp.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class LogInSignUp extends Component {
+	render() {
+		return <div>Login</div>;
+	}
+}
+
+export default LogInSignUp;
