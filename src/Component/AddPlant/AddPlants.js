73ba08aad9deefe99f52d42faed9bdd0a9a80ee0
lesley <lesleybanadzem2017@gmail.com>
2021-04-15 14:59:52 -0700

    Added a form field to seach plants and hookup the field to set state

--- src/Component/AddPlant/AddPlants.js
+++ src/Component/AddPlant/AddPlants.js
@@ -2,11 +2,39 @@ import React, { Component } from "react";
 import Header from "../Header/Header";
 
 export default class AddPlants extends Component {
+	constructor() {
+		super();
+		this.state = {
+			plantName: "",
+		};
+	}
 	render() {
+		console.log(this.state);
 		return (
 			<>
 				<Header />
-				<section>Add plant</section>
+				<section>
+					<section className="plant_search_input_field w-3/4  my-10 mx-auto">
+						<div className="">
+							<label
+								className="block text-th-primary text-lg font-bold mb-3"
+								htmlFor="username"
+							>
+								Search Plant
+							</label>
+							<input
+								className="shadow appearance-none border rounded w-full py-3 px-3 text-grey-darker active:outline-none text-xl"
+								id="username"
+								type="text"
+								name="plant_name"
+								onChange={(event) =>
+									this.setState({ plantName: event.target.value })
+								}
+								placeholder="Type in plant name"
+							/>
+						</div>
+					</section>
+				</section>
 			</>
 		);
 	}
